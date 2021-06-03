646f2fea538021e098255111ca7561d585bdacf4
DKendle <dina.kendle@gmail.com>
2021-06-03 15:21:38 -0400

    Scratch Code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+let ul = document.getElementById("tasks")
+let li = document.createElement("li")
+let form = document.getElementById("create-task-form")
+let inputValue = document.getElementById("new-task-description").value
+
+form.addEventListener("submit", function(event){
+  event.preventDefault()
+  li.innerHTML = inputValue
+  ul.appendChild(li)
+})
